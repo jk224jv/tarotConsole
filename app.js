bbcf13691fa6a82c6ae6a9fa413274a662d79908
Jimmy Karlsson <jk224jv@student.lnu.se>
2022-09-15 11:47:30 +0200

    WIP functions commented

--- app.js
+++ app.js
@@ -4,3 +4,16 @@
  * 
  * @author Jimmy Karlsson <jk224jv@strudent.lnu.se>
  */
+
+//Make a display table, a preformated string... 
+// have a gap for 3 cards
+
+// create deck as an object
+// 4 suits 1-10 + page + knight + queen + King
+// 16 Major arcana
+
+// create method to "scramble the deck and pull 3"
+
+// create method to display the "table and the choosen cards"
+
+
